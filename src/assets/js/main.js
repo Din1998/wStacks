63e0e6496ffb5a03223cb85d09165380e957a638
Din1998 <dinislam8803@gmail.com>
2024-03-10 18:58:16 +0600

    finish index page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -372,7 +372,9 @@
       speed: 300,
       slidesToShow: 1,
       slidesToScroll: 1,
-      arrows: false,
+      arrows: true,
+      prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+      nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
       responsive: [
         {
           breakpoint: 1100,
